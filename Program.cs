d580d2b6aea90b461938481d411c6ee487776c09
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:21:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int sum = 0;
+        int min = int.MaxValue;
+        int max = int.MinValue;
+        int product = 1;
+
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Введіть число {0}: ", i + 1);
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            
+            sum += number;
+            if (number < min)
+            {
+                min = number;
+            }
+            if (number > max)
+            {
+                max = number;
+            }
+            product *= number;
+        }
+        Console.WriteLine("Сума: " + sum);
+        Console.WriteLine("Максимум: " + max);
+        Console.WriteLine("Мінімум: " + min);
+        Console.WriteLine("Добуток: " + product);
+    }
+}
